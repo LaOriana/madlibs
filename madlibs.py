419fb080f794f05931499ec9ce188536ef7cdaa1
Oriana Laura K González <orianalk@gmail.com>
2021-03-02 22:11:57 +0000

    Create a variable for the list of colors and update return to include it

--- madlibs.py
+++ madlibs.py
@@ -58,15 +58,18 @@ def show_madlib_form():
 def show_madlib():
 
     person = request.args.get("person")
-    color = request.args.get("color")
+    colors = request.args.get("colors")
     noun = request.args.get("noun")
     adjective = request.args.get("adjective")
 
+    color_list = request.args.get("colors")
+
     return render_template("madlib.html",
                             person=person,
-                            color=color,
+                            colors=colors,
                             noun=noun,
                             adjective=adjective
+                            color_list=color_list
                             )
 
 
